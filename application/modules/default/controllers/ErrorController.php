a14bdd8253a5106d74f7f1b5e8ebf825f1c4daee
Jack He <y2khjh@hotmail.com>
2012-08-28 21:19:59 +1000

    new modules

---
+++ application/modules/default/controllers/ErrorController.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'Zend/Controller/Action.php';
+
+class ErrorController extends Zend_Controller_Action
+{
+    public function errorAction()
+    {
+
+    }
+}
---
+++ application/modules/user/controllers/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'Zend/Controller/Action.php';
+
+class User_IndexController extends Zend_Controller_Action
+{
+    public function indexAction()
+    {
+
+    }
+}
